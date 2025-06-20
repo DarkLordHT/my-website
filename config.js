9199b529ff99c5db6e4bca6caef9cd109700d073
DarkLordHT <elvinboonjunhao2010@gmail.com>
2025-06-20 23:42:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const repoOwner = 'darklordht'; // 替换为你的 GitHub 用户名
 const repoName  = 'my-website'; // 替换为你的 GitHub 仓库名
 const dataFilePath = 'data/students.json';
 const branch = 'main';
-const GITHUB_TOKEN = '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';  // 推荐用 localStorage 设置，不要直接写 token
+
